c98dbbf1c7a811f0a520c96dd58f3d93904336c3
Vanna Winland <vgwinland@gmail.com>
2019-10-10 22:28:10 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,8 +17,8 @@ class Doctor
   end
   
   def appointments
-    Appointment.all.select do |appointment|
-      appointment.doctor == self
+    Appointment.all.select {|appointment|
+      appointment.doctor == self}
     end
   end
   
