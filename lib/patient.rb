9a4f72f937165f82354de598eba559edc8f6143a
Vanna Winland <vgwinland@gmail.com>
2019-10-10 21:57:36 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,8 +17,9 @@ class Patient
   end
   
   def appointments 
-    Appointment.all.select {|appointment|
-      appointment.patient == self}
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
   end
   
   def doctors
